d4e0a070a98de88d94c73d4e8e31f3fffb1dab95
Quame Jnr <kaboateng14@gmail.com>
2020-02-21 13:38:53 +0000

    Queue time function

--- Misc/codewars.py
+++ Misc/codewars.py
@@ -1,5 +1,6 @@
 from Decorators.Decorators import my_timer
 import time
+import heapq
 
 # function to return if the sum of an array is even or odd
 def odd_or_even(arr):
@@ -55,6 +56,80 @@ def disemvowel(string):
 # a walk around a city block that takes exactly 10 minutes and returns you to where you started
 def is_valid_walk(walk):
     return len(walk) == 10 and walk.count('n') == walk.count('s') and walk.count('w') == walk.count('e')
+
+
+def rgb(r, g, b):
+    hex ={0: '0', 1: '1', 2: '2', 3: '3',
+          4: '4', 5: '5', 6: '6', 7: '7',
+          8: '8', 9: '9', 10: 'A', 11: 'B',
+          12: 'C', 13: 'D', 14: 'E', 15: 'F'}
+
+
+    code=[]
+
+    if all([r, g, b]):
+
+        while r > 16:
+            num = r % 16
+            code.append(num)
+            r = r//16
+        code.append(r)
+
+        while g > 16:
+            num = g % 16
+            code.append(num)
+            g = g//16
+        code.append(g)
+
+        while b > 16:
+            num = b % 16
+            code.append(num)
+            b = b//16
+        code.append(b)
+
+        reverse = code[::-1]
+        new_code = [hex.get(key) for key in reverse]
+        return "".join(new_code)
+    return '0' * 6
+
+# finding number of cubes for building a pile of cubes given the volume of cube
+
+def find_nb(m):
+    n, res = 1, 1
+    while res < m:
+        n += 1
+        res += n**3
+    return n if res == m else -1
+
+
+import random
+array = []
+
+while len(array)<300000:
+    array.append(random.randint(1, 20))
+
+array = array
+
+
+# queue time taken for customers to check-out of the supermarket given the number of checkout tills
+@my_timer
+def queue_time1(customers, n):
+    if len(customers) != 0:
+        new_lst, b = sorted(customers[:n]), customers[n:]
+        for i in b:
+            new_lst[0] += i
+            new_lst = sorted(new_lst)
+        return new_lst[-1]
+    return 0
+
+
+# alternate code to previous code and faster
+def queue_time2(customers, n):
+    heap = [0] * n
+    for time in customers:
+        heapq.heapreplace(heap, heap[0] + time)
+    return max(heap)
+
 
 
 
